83add54311bf81dc2ee45e2ef714f47c71e16d59
nylander <j.a.a.nylander@gmail.com>
2015-12-17 09:37:19 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,118 @@
+# Easy Web Share on UPPMAX
+
+By: Johan.Nylander\@bils.se
+
+Version: 12/17/2015 09:12:09 AM
+
+File: `/proj/b2011210/dlbin/webshare.sh`
+
+See also: [https://www.uppmax.uu.se/webexport-guide]([https://www.uppmax.uu.se/webexport-guide)
+
+---
+
+#### For the Impatient:
+
+    mkdir -p /proj/b123456/webexport/share
+    cd /proj/b123456/webexport/share
+    cp -r /path/to/some_folder_or_file .
+    /proj/b2011210/dlbin/webshare.sh
+
+---
+
+## HOWTO 
+
+In this example we wish to share sequences delivered to your project's
+INBOX (folder `/proj/b123456/INBOX/My.Name_15_00`) so it can be accessable
+on the URL [http://export.uppmax.uu.se/b123456/seqs](http://export.uppmax.uu.se/b123456/seqs).
+Note that you need to change `b123456` and `My.Name_15_00` to fit your needs.
+
+### 1. Set PATH in order to find the script
+
+You can run the script by specifying the full path, or alternativelly, make sure you have
+the `/proj/b2011210/dlbin` directory in your PATH variable:
+
+    export PATH=$PATH:/proj/b2011210/dlbin
+
+(The above line can also be included in your `.bash_profile`).
+
+The full path to the script is `/proj/b2011210/dlbin/webshare.sh`.
+
+
+### 2. Create a folder to share
+
+Files to be shared from UPPMAX need to be in a special place in order for
+the webserver to see them. This means that we need to put files from our INBOX to a
+folder inside a `webexport` folder. In this example, we wish the shared folder to be
+named `seqs`, and this folder need to be inside the `/proj/b123456/webexport` folder.
+If you don't already have the `webexport` folder, you may create it first,
+or simply do:
+
+    mkdir -p /proj/b123456/webexport/seqs
+
+
+### 3. Copy files to be shared to the newly created folder 
+
+**Note**: this applies specifically for files and folders that will be transferred
+from the `INBOX` - you need to use the `cp` command in order to correctly change the
+ownerships of the transferred files! Normally you would have used, e.g., `mv`, but this
+will not work on UPPMAX (UPPMAX's `mv` changes ownerships correctly for files, but not
+for folders). Furthermore, the files need to be readable by the webserver, so symbolic
+links will not work.
+
+So to copy a whole folder, use:
+
+    cp -r /proj/b123456/INBOX/My.Name_15_00 /proj/b123456/webexport/seqs
+
+
+### 4. Run the `webshare.sh` script to create a password protected URL
+
+Easiest is to `cd` into the folder to be shared:
+
+    cd /proj/b123456/webexport/seqs
+
+and then run the script without arguments:
+
+    webshare.sh
+
+(or, if the script is not found in the PATH, try `/proj/b2011210/dlbin/webshare.sh`)
+
+### 5. Share the link!
+
+The output from the script looks something like this:
+
+    folder to share (set by pwd): /proj/b123456/webexport/seqs
+         URL: http://export.uppmax.uu.se/b123456/seqs
+        User: kxlvtr
+    Password: cvtddq
+    
+Make sure you write down the information and then share it.
+
+When some one attempts to access the URL
+[http://export.uppmax.uu.se/b123456/seqs](http://export.uppmax.uu.se/b123456/seqs),
+they will be prompted for a User Name (`kxlvtr`), and a Password (`cvtddq`).
+
+---
+
+## The `webshare.sh` script:
+
+#### Usage
+
+    webshare.sh [-h] [-f folder] [-u user]
+
+#### Description
+
+    Webshare files in /proj/<projid>/webshare/<folder>.
+    The URL (password protected) will be http://export.uppmax.uu.se/<projid>/<folder>
+    Usage:
+        First, create <folder> with content to be shared.
+        Then, cd to <folder> and run:
+
+        webshare.sh
+
+    The user and password will be written to stdout. Write them down.
+    Optionally, both <folder> and <user> can be given as arguments:
+
+        webshare.sh -f <folder> -u <user>
+
+    The <folder> will be created if not already present.
+
---
+++ webshare.sh
@@ -0,0 +1,156 @@
+#!/bin/bash -l
+
+## Description: Quick webshare a folder on UPPMAX.
+##              Folder needs to be in /proj/<projid>/webexport/.
+##              All files need to be readable by all on the server
+##              (chmod -R a+r /proj/<projid>/webshare/<folder>).
+##              Access to URL will be password protected.
+##              Password and, optimally, user, will be set randomly.
+## Folder:      /proj/<projid>/webexport/<folder>
+## URL:         http://export.uppmax.uu.se/<projid>/<folder>
+## Usage:       webshare.sh [-f folder] [-u user] [-h]
+## By:          Johan Nylander, BILS
+## Version:     12/17/2015 09:25:54 AM
+
+## Check if we can run on system (UPPMAX)
+if [ ! -d "/proj" ] ; then
+    echo "Can not find /proj directory on this system."
+    exit 1
+fi
+if [ ! -r "/dev/urandom" ] ; then
+    echo "Can not read file /dev/urandom on this system."
+    exit 1
+fi
+
+## Check arguments, "space style" ( proa.sh -f arg -b arg)
+while [[ $# > 1 ]]
+do
+key="$1"
+
+case $key in
+    -f|--folder)
+    FOLDER="$2"
+    shift
+    ;;
+    -d|--dir)
+    FOLDER="$2"
+    shift
+    ;;
+    -n|--name)
+    NAME="$2"
+    shift
+    ;;
+    -u|--user)
+    NAME="$2"
+    shift
+    ;;
+    -p|--project)
+    PROJID="$2"
+    shift
+    ;;
+    -h|--help)
+    echo "Webshare files in /proj/<projid>/webshare/<folder>."
+    echo "The URL (password protected) will be http://export.uppmax.uu.se/<projid>/<folder>"
+    echo "Usage:"
+    echo "    First, create <folder> with content to be shared."
+    echo "    Then, cd to <folder> and run:"
+    echo ""
+    echo "        webshare.sh"
+    echo ""
+    echo "    The user and password will be written to stdout. Write them down."
+    echo "    Optionally, both <folder> and <user> can be given as arguments:"
+    echo ""
+    echo "        webshare.sh -f <folder> -u <user>"
+    echo ""
+    echo "    The <folder> will be created if not already present."
+    exit 1
+    ;;
+    *)
+    echo "Unknown argument"
+    echo "Usage: webshare.sh [-h] [-f folder] [-u user]"
+    exit 1
+    ;;
+esac
+shift
+done
+
+## Set project id
+if [ -z "$PROJID" ]; then
+    ## Looks for b2014211 in /proj/b2014211/...
+    ## Need to be in the project tree to work (since using pwd)
+    PROJID=$(pwd | sed -e 's@/proj/\([^/]*\)/.*$@\1@')
+fi
+if [ ! -e "/proj/$PROJID" ] ; then
+    echo "Can not find folder /proj/$PROJID. Is project ID correct?"
+    exit 1
+fi
+
+## Set folder
+WEBDIR="/proj/$PROJID/webexport"
+if [ -n "$FOLDER" ]; then
+    if [ -e "$WEBDIR/$FOLDER" ] ; then
+        echo "folder exists in $WEBDIR: $(realpath $FOLDER)"
+    else
+        echo "folder does not exist in $WEBDIR"
+        FOLDER=$WEBDIR/$FOLDER
+        mkdir -v -p $FOLDER
+        chmod -v -R a+r $FOLDER
+        if [ ! -e "$FOLDER" ] ; then
+            echo "failed to create $FOLDER"
+            exit 1
+        fi
+    fi
+else
+    FOLDER=$(pwd)
+    F=$(basename $FOLDER)
+    if [[ "$FOLDER" == $WEBDIR/$F ]] ; then
+        echo "folder to share (set by pwd): $FOLDER"
+    else
+        echo "$FOLDER is not inside $WEBDIR ?"
+        echo "Need to run the script from inside the directory to be shared if run without arguments."
+        exit 1
+    fi
+fi
+
+## Set user
+if [ -n "$NAME" ]; then
+    echo ""
+    #echo "name set manually: $NAME"
+else
+    NAME=$(< /dev/urandom tr -dc a-z | head -c6)
+    #echo "name set randomly: $NAME"
+fi
+
+## Create password
+PWD=$(< /dev/urandom tr -dc a-z | head -c6)
+#echo "generated password: $PWD"
+
+## Create .htpasswd. If file exists, add user.
+if [ -e "$FOLDER/.htpasswd" ] ;then
+    echo -e "$NAME:$(perl -le 'print crypt("$ENV{PWD}","moresalt")')" >> $FOLDER/.htpasswd
+else
+    echo -e "$NAME:$(perl -le 'print crypt("$ENV{PWD}","moresalt")')" > $FOLDER/.htpasswd
+fi
+
+## Create .htaccess. If file exists, do nothing.
+if [ ! -e "$FOLDER/.htaccess" ] ;then
+    touch $FOLDER/.htaccess
+    echo "Options +Indexes"                  >> $FOLDER/.htaccess
+    echo "AuthType Basic"                    >> $FOLDER/.htaccess
+    echo "AuthUserFile $FOLDER/.htpasswd"    >> $FOLDER/.htaccess
+    echo "AuthName \"Private project area\"" >> $FOLDER/.htaccess
+    echo "Require valid-user"                >> $FOLDER/.htaccess
+fi
+
+## Set files readable to all
+chmod -R ugo+r $FOLDER
+
+## Make sure folders have o+x
+find $FOLDER -type d -exec chmod 755 {} +
+
+## Print user+passwd to stdout. Write them down.
+echo "     URL: http://export.uppmax.uu.se/$PROJID/$F"
+echo "    User: $NAME"
+echo "Password: $PWD"
+
+ 
